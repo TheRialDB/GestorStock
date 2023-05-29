5dbc25cf94a4e14ed4699b140f3b080f694f99fa
david alejandro izquierdo cafaro <davidbeisbol@hotmail.com.ar>
2023-05-29 18:23:04 -0300

    update clase usuario

--- Server/Controllers/UsuarioController.cs
+++ Server/Controllers/UsuarioController.cs
@@ -1,33 +1,33 @@
-﻿using GestorStock.BD.Data;
-using GestorStock.BD.Data.Entity;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+﻿//using GestorStock.BD.Data;
+//using GestorStock.BD.Data.Entity;
+//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.EntityFrameworkCore;
 
-namespace GestorStock.Server.Controllers
-{
-    public class UsuarioController : Controller
-    {
-        public IActionResult Index()
-        {
-            return View();
-        }
-        Context _context;
-        public UsuarioController(Context context)
-        {
-            _context = context;
-        }
+//namespace GestorStock.Server.Controllers
+//{
+//    public class UsuarioController : Controller
+//    {
+//        public IActionResult Index()
+//        {
+//            return View();
+//        }
+//        Context _context;
+//        public UsuarioController(Context context)
+//        {
+//            _context = context;
+//        }
 
 
-        [HttpPost]
-        public async Task<ActionResult<Usuario>> PostTodoItem(Usuario nuevoUsuario)
-        {          
+//        [HttpPost]
+//        public async Task<ActionResult<Usuario>> PostTodoItem(Usuario nuevoUsuario)
+//        {          
 
-            _context.Add<Usuario>(nuevoUsuario);
-            await _context.SaveChangesAsync();
+//            _context.Add<Usuario>(nuevoUsuario);
+//            await _context.SaveChangesAsync();
 
-            //return CreatedAtAction("GetTodoItem", new { id = todoItem.Id }, todoItem);
-            return CreatedAtAction(nameof(GetTodoItem), new { id = todoItem.Id }, todoItem);
-        }
+//            //return CreatedAtAction("GetTodoItem", new { id = todoItem.Id }, todoItem);
+//            return CreatedAtAction(nameof(GetTodoItem), new { id = todoItem.Id }, todoItem);
+//        }
 
-    }
-}
+//    }
+//}
