5e795a6433394e5a29d2d6fc6899f54223f4a740
david alejandro izquierdo cafaro <davidbeisbol@hotmail.com.ar>
2023-05-29 17:27:37 -0300

    logica de registro de usuario

---
+++ Server/Controllers/UsuarioController.cs
@@ -0,0 +1,33 @@
+﻿using GestorStock.BD.Data;
+using GestorStock.BD.Data.Entity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GestorStock.Server.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        Context _context;
+        public UsuarioController(Context context)
+        {
+            _context = context;
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult<Usuario>> PostTodoItem(Usuario nuevoUsuario)
+        {          
+
+            _context.Add<Usuario>(nuevoUsuario);
+            await _context.SaveChangesAsync();
+
+            //return CreatedAtAction("GetTodoItem", new { id = todoItem.Id }, todoItem);
+            return CreatedAtAction(nameof(GetTodoItem), new { id = todoItem.Id }, todoItem);
+        }
+
+    }
+}
