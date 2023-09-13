28f1f882f204bd7db71767ba3e458eb6f1986202
Ramiro Iriarte <ramiro_iriarte003@hotmail.com>
2023-09-13 11:15:10 -0300

    Controladores y DTOS

--- Client/Extensiones/AutenticacionExtension.cs
+++ Client/Extensiones/AutenticacionExtension.cs
@@ -5,59 +5,59 @@ using System.Security.Claims;
 
 namespace GestorStock.Client.Extensiones
 {
-    public class AutenticacionExtension : AuthenticationStateProvider
-    {
-        private readonly ISessionStorageService _sessionStorage;
-        private ClaimsPrincipal _sinInformacion = new ClaimsPrincipal(new ClaimsIdentity());
+    //public class AutenticacionExtension : AuthenticationStateProvider
+    //{
+    //    private readonly ISessionStorageService _sessionStorage;
+    //    private ClaimsPrincipal _sinInformacion = new ClaimsPrincipal(new ClaimsIdentity());
 
-        public AutenticacionExtension(ISessionStorageService sessionStorage)
-        {
-            _sessionStorage = sessionStorage;
-        }
+    //    public AutenticacionExtension(ISessionStorageService sessionStorage)
+    //    {
+    //        _sessionStorage = sessionStorage;
+    //    }
 
-        public async Task ActualizarEstadoAutenticacion(SesionDTO? sesionUsuario)
-        {
-            ClaimsPrincipal claimsPrincipal;
+    //    public async Task ActualizarEstadoAutenticacion(SesionDTO? sesionUsuario)
+    //    {
+    //        ClaimsPrincipal claimsPrincipal;
 
-            if (sesionUsuario != null)
-            {
-                claimsPrincipal = new ClaimsPrincipal(new ClaimsIdentity(new List<Claim>
-                {
-                    new Claim(ClaimTypes.Name,sesionUsuario.Nombre),
-                    new Claim(ClaimTypes.Email,sesionUsuario.Correo),
-                    new Claim(ClaimTypes.Role,sesionUsuario.Rol)
+    //        if (sesionUsuario != null)
+    //        {
+    //            claimsPrincipal = new ClaimsPrincipal(new ClaimsIdentity(new List<Claim>
+    //            {
+    //                new Claim(ClaimTypes.Name,sesionUsuario.Nombre),
+    //                new Claim(ClaimTypes.Email,sesionUsuario.Correo),
+    //                new Claim(ClaimTypes.Role,sesionUsuario.Rol)
 
-                }, "JwtAuth"));
+    //            }, "JwtAuth"));
 
-                await _sessionStorage.GuardarStorage("sesionUsuario", sesionUsuario);                
-            }
-            else
-            {
-                claimsPrincipal = _sinInformacion;
-                await _sessionStorage.RemoveItemAsync("sesionUsuario");
-            }
+    //            await _sessionStorage.GuardarStorage("sesionUsuario", sesionUsuario);                
+    //        }
+    //        else
+    //        {
+    //            claimsPrincipal = _sinInformacion;
+    //            await _sessionStorage.RemoveItemAsync("sesionUsuario");
+    //        }
 
-            NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(claimsPrincipal)));
-        }
+    //        NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(claimsPrincipal)));
+    //    }
 
 
-        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
-        {
-            var sesionUsuario = await _sessionStorage.ObtenerStorage<SesionDTO>("sesionUsuario");
+    //    public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+    //    {
+    //        var sesionUsuario = await _sessionStorage.ObtenerStorage<SesionDTO>("sesionUsuario");
 
-            if (sesionUsuario == null)
-                return await Task.FromResult(new AuthenticationState(_sinInformacion));
+    //        if (sesionUsuario == null)
+    //            return await Task.FromResult(new AuthenticationState(_sinInformacion));
 
-            var claimsPrincipal = new ClaimsPrincipal(new ClaimsIdentity(new List<Claim>
-                {
-                    new Claim(ClaimTypes.Name,sesionUsuario.Nombre),
-                    new Claim(ClaimTypes.Email,sesionUsuario.Correo),
-                    new Claim(ClaimTypes.Role,sesionUsuario.Rol)
+    //        var claimsPrincipal = new ClaimsPrincipal(new ClaimsIdentity(new List<Claim>
+    //            {
+    //                new Claim(ClaimTypes.Name,sesionUsuario.Nombre),
+    //                new Claim(ClaimTypes.Email,sesionUsuario.Correo),
+    //                new Claim(ClaimTypes.Role,sesionUsuario.Rol)
 
-                }, "JwtAuth"));
+    //            }, "JwtAuth"));
 
-            return await Task.FromResult(new AuthenticationState(claimsPrincipal));
+    //        return await Task.FromResult(new AuthenticationState(claimsPrincipal));
 
-        }
-    }
+    //    }
+    //}
 }
