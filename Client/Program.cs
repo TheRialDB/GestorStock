d55b07e18de251d09b86ee3cef1694486cd49cfd
david alejandro izquierdo cafaro <davidbeisbol@hotmail.com.ar>
2023-09-21 15:40:39 -0300

    resolucion de problemas al inicio del programa

--- Client/Program.cs
+++ Client/Program.cs
@@ -2,19 +2,10 @@ using GestorStock.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-using Blazored.SessionStorage;
-using Microsoft.AspNetCore.Components.Authorization;
-using GestorStock.Client.Extensiones;
-
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-builder.Services.AddBlazoredSessionStorage();
-//builder.Services.AddScoped<AuthenticationStateProvider, AutenticacionExtension>();
-builder.Services.AddAuthorizationCore();
-
 
 await builder.Build().RunAsync();
