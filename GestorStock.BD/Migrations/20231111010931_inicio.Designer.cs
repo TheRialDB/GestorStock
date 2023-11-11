88ba292c66b036cd49c6c44a1b8726ed34e20e6e
Thomas Dergazarian <Dergazarian21@hotmail.com>
2023-11-10 22:36:17 -0300

    notapedido (No sirve)

--- GestorStock.BD/Migrations/20231111010931_inicio.Designer.cs
+++ GestorStock.BD/Migrations/20231111010931_inicio.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GestorStock.BD.Migrations
 {
     [DbContext(typeof(Context))]
-    [Migration("20231110205217_inicio")]
+    [Migration("20231111010931_inicio")]
     partial class inicio
     {
         /// <inheritdoc />
